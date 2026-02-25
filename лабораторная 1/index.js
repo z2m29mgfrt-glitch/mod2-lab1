e4295fda4eefcdaeb4102c37dffaae9590aed62d
z2m29mgfrt-glitch <z2m29mgfrt@privaterelay.appleid.com>
2026-02-25 08:43:09 +0400

    lab2-1

---
+++ лабораторная 1/index.js
@@ -0,0 +1,36 @@
+let myName = 'Екатерина';
+console.log(myName);
+
+let mySurname = 'Лысова';
+console.log(mySurname);
+
+let isBachelor = true;
+console.log(isBachelor);
+
+let myAge = 23;
+console.log(myAge);
+
+let myFullName = myName + ' ' + mySurname;
+myFullName = myFullName + ' Возраст: ' + myAge;
+
+console.log(myFullName);
+
+
+
+let currentHour = 14;
+let isWorkingHours = currentHour >= 9 && currentHour <= 18;
+let todayDay = 'Вторник';
+let isHoliday = todayDay === 'Суббота' || todayDay === 'Воскресенье';
+let canWork = isWorkingHours && !isHoliday;
+console.log(canWork);
+
+
+let celsius = prompt("Введите температуру в Цельсиях:");
+celsius = Number(celsius);
+
+if (isNaN(celsius)) {
+    alert("Ошибка: введите корректное число!");
+} else {
+    const fahrenheit = celsius * 9/5 + 32;
+    alert(fahrenheit);
+}
